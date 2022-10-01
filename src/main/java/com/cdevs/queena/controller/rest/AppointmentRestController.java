138724497f263df3efc4a42168e2766d272d819c
CHECH01 <ss_1913@ooutlook.com>
2022-10-01 18:23:09 -0500

    generic rest controller implemented

--- src/main/java/com/cdevs/queena/controller/rest/AppointmentRestController.java
+++ src/main/java/com/cdevs/queena/controller/rest/AppointmentRestController.java
@@ -1,55 +1,22 @@
 package com.cdevs.queena.controller.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.cdevs.queena.commons.GenericServiceApi;
 import com.cdevs.queena.model.Appointment;
 import com.cdevs.queena.service.api.AppointmentServiceAPI;
 
 @RestController
-@RequestMapping("api/v1/")
-public class AppointmentRestController {
+@RequestMapping(value = GenericRestController.BASE_URL + "appointment")
+public class AppointmentRestController extends GenericRestController<Appointment,Long>{
+
     @Autowired
     private AppointmentServiceAPI service;
 
-    @GetMapping("/all-appointments")
-    public List<Appointment> getAll(Model model){
-        return service.getAll();
-    }
-
-    @PostMapping("/save-appointment")
-    public ResponseEntity<Appointment> save(@RequestBody Appointment appointment){
-        Appointment a = service.save(appointment); 
-        return new ResponseEntity<Appointment>(a, HttpStatus.OK);
-    }
-
-    @GetMapping("/find-appointment/{id}")
-    public ResponseEntity<Appointment> get(@PathVariable Long id, Model model){
-        Appointment a = service.get(id);
-        if(a != null)
-            service.get(id);
-        else
-            return new ResponseEntity<Appointment>(a,HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<Appointment>(a,HttpStatus.OK);
-    }
-
-    @GetMapping("/delete-appointment/{id}")
-    public ResponseEntity<Appointment> delete(@PathVariable Long id){
-        Appointment a = service.get(id);
-        if(a != null)
-            service.delete(id);
-        else
-            return new ResponseEntity<Appointment>(a,HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<Appointment>(a,HttpStatus.OK);
+    @Override
+    public GenericServiceApi<Appointment, Long> getService() {
+        return service;
     }
 }
--- src/main/java/com/cdevs/queena/controller/rest/ClientRestController.java
+++ src/main/java/com/cdevs/queena/controller/rest/ClientRestController.java
@@ -1,57 +1,22 @@
 package com.cdevs.queena.controller.rest;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+import com.cdevs.queena.commons.GenericServiceApi;
 import com.cdevs.queena.model.Client;
 import com.cdevs.queena.service.api.ClientServiceAPI;
 
 @RestController
-@RequestMapping("api/v1/")
-public class ClientRestController {
+@RequestMapping(value = GenericRestController.BASE_URL + "client")
+public class ClientRestController extends GenericRestController<Client,Long>{
 
     @Autowired
     private ClientServiceAPI service;
-
-    @GetMapping("/all-clients")
-    public List<Client> getAll(Model model){
-        return service.getAll();
-    }
-
-    @PostMapping("/save-client")
-    public ResponseEntity<Client> save(@RequestBody Client client){
-        Client c = service.save(client); 
-        return new ResponseEntity<Client>(c, HttpStatus.OK);
-    }
-
-    @GetMapping("/find-client/{id}")
-    public ResponseEntity<Client> get(@PathVariable Long id, Model model){
-        Client c = service.get(id);
-        if(c != null)
-            service.get(id);
-        else
-            return new ResponseEntity<Client>(c,HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<Client>(c,HttpStatus.OK);
-    }
-
-    @GetMapping("/delete-client/{id}")
-    public ResponseEntity<Client> delete(@PathVariable Long id){
-        Client c = service.get(id);
-        if(c != null)
-            service.delete(id);
-        else
-            return new ResponseEntity<Client>(c,HttpStatus.INTERNAL_SERVER_ERROR);
-        return new ResponseEntity<Client>(c,HttpStatus.OK);
+    
+    @Override
+    public GenericServiceApi<Client, Long> getService() {
+        return service;
     }
 }
